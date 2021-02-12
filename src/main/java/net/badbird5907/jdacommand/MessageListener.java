681ea8f77fd60f37dda6a464d63b4eb53ae832b1
Chinay-Domitrix <55807470+Chinay1023@users.noreply.github.com>
2021-02-12 16:03:05 -0500

    formatting

--- src/main/java/net/badbird5907/jdacommand/MessageListener.java
+++ src/main/java/net/badbird5907/jdacommand/MessageListener.java
@@ -3,19 +3,15 @@ package net.badbird5907.jdacommand;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MessageListener extends ListenerAdapter {
-    @Override
-    public void onMessageReceived(MessageReceivedEvent e){
-        if(e.getMessage().getContentRaw().startsWith(OctoCommand.getInstance().prefix)){
-            String[] args1 = e.getMessage().getContentRaw().split(" ");
-            String a = e.getMessage().getContentRaw().replaceFirst(args1[0], "");
-            String[] args = a.split(" ");
-            OctoCommand.commands.forEach(cmd ->{
-                if(cmd.name.equalsIgnoreCase(OctoCommand.getInstance().prefix + args1[0].replaceFirst(OctoCommand.getInstance().prefix, ""))){
-                    cmd.execute(args, new CommandEvent(args, e.getChannel(), e.getMessage(), e));
-                }
-            });
-        }
-    }
+import static net.badbird5907.jdacommand.OctoCommand.*;
 
+public class MessageListener extends ListenerAdapter {
+	@Override
+	public void onMessageReceived(MessageReceivedEvent e) {
+		if (e.getMessage().getContentRaw().startsWith(getInstance().prefix)) {
+			String[] args1 = e.getMessage().getContentRaw().split(" ");
+			String[] args = e.getMessage().getContentRaw().replaceFirst(args1[0], "").split(" ");
+			commands.stream().filter(cmd -> cmd.name.equalsIgnoreCase(getInstance().prefix + args1[0].replaceFirst(getInstance().prefix, ""))).forEach(cmd -> cmd.execute(args, new CommandEvent(args, e.getChannel(), e.getMessage(), e)));
+		}
+	}
 }
--- src/main/java/net/badbird5907/jdacommand/OctoCommand.java
+++ src/main/java/net/badbird5907/jdacommand/OctoCommand.java
@@ -5,19 +5,21 @@ import net.dv8tion.jda.api.JDA;
 import java.util.ArrayList;
 
 public class OctoCommand {
-    public String prefix = null;
-    public JDA jda = null;
-    private static OctoCommand instance;
-    public OctoCommand(String prefix, JDA jda){
-        this.prefix = prefix;
-        this.jda = jda;
-    }
-    public static ArrayList<Command> commands = new ArrayList<>();
-    public void init() {
-        instance = this;
-    }
+	public static final ArrayList<Command> commands = new ArrayList<>();
+	private static OctoCommand instance;
+	public final String prefix;
+	public final JDA jda;
 
-    public static OctoCommand getInstance() {
-        return instance;
-    }
+	public OctoCommand(String prefix, JDA jda) {
+		this.prefix = prefix;
+		this.jda = jda;
+	}
+
+	public static OctoCommand getInstance() {
+		return instance;
+	}
+
+	public void init() {
+		instance = this;
+	}
 }
