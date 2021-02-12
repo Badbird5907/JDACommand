30fb69937ce3e797f217313ffbd15f064bf89681
Chinay-Domitrix <55807470+Chinay1023@users.noreply.github.com>
2021-02-12 16:03:05 -0500

    formatting

--- src/main/java/net/badbird5907/jdacommand/CommandEvent.java
+++ src/main/java/net/badbird5907/jdacommand/CommandEvent.java
@@ -9,15 +9,15 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 @Getter
 @Setter
 public class CommandEvent {
-    private String[] args = null;
-    private MessageChannel channel;
-    private Message message;
-    private MessageReceivedEvent event;
+	private String[] args;
+	private MessageChannel channel;
+	private Message message;
+	private MessageReceivedEvent event;
 
-    public CommandEvent(final String args[], final MessageChannel channel, final Message message, final MessageReceivedEvent event){
-        this.args = args;
-        this.channel = channel;
-        this.message = message;
-        this.event = event;
-    }
+	public CommandEvent(final String[] args, final MessageChannel channel, final Message message, final MessageReceivedEvent event) {
+		this.args = args;
+		this.channel = channel;
+		this.message = message;
+		this.event = event;
+	}
 }
