213ead97fe116e2d04954ea40fc8f82e9f4fc9e5
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2021-02-12 16:26:24 -0500

    done?

--- src/main/java/net/badbird5907/jdacommand/JDACommand.java
+++ src/main/java/net/badbird5907/jdacommand/JDACommand.java
@@ -4,20 +4,24 @@ import net.dv8tion.jda.api.JDA;
 
 import java.util.ArrayList;
 
-public class OctoCommand {
+public class JDACommand {
     public String prefix = null;
     public JDA jda = null;
-    private static OctoCommand instance;
-    public OctoCommand(String prefix, JDA jda){
+    private static JDACommand instance;
+    public JDACommand(String prefix, JDA jda){
         this.prefix = prefix;
         this.jda = jda;
     }
     public static ArrayList<Command> commands = new ArrayList<>();
     public void init() {
         instance = this;
+        jda.addEventListener(new MessageListener());
+    }
+    public static void registerCommand(Command command){
+        commands.add(command);
     }
 
-    public static OctoCommand getInstance() {
+    public static JDACommand getInstance() {
         return instance;
     }
 }
--- src/main/java/net/badbird5907/jdacommand/MessageListener.java
+++ src/main/java/net/badbird5907/jdacommand/MessageListener.java
@@ -6,12 +6,12 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent e){
-        if(e.getMessage().getContentRaw().startsWith(OctoCommand.getInstance().prefix)){
+        if(e.getMessage().getContentRaw().startsWith(JDACommand.getInstance().prefix)){
             String[] args1 = e.getMessage().getContentRaw().split(" ");
             String a = e.getMessage().getContentRaw().replaceFirst(args1[0], "");
             String[] args = a.split(" ");
-            OctoCommand.commands.forEach(cmd ->{
-                if(cmd.name.equalsIgnoreCase(OctoCommand.getInstance().prefix + args1[0].replaceFirst(OctoCommand.getInstance().prefix, ""))){
+            JDACommand.commands.forEach(cmd ->{
+                if(cmd.name.equalsIgnoreCase(JDACommand.getInstance().prefix + args1[0].replaceFirst(JDACommand.getInstance().prefix, ""))){
                     cmd.execute(args, new CommandEvent(args, e.getChannel(), e.getMessage(), e));
                 }
             });
