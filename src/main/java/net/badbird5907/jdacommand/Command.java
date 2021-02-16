5c1c0b63861d2119943fe8bccc77f66ad1b68a36
Chinay-Domitrix <55807470+Chinay1023@users.noreply.github.com>
2021-02-15 21:38:41 -0500

    formatting and static imports for the most part

--- src/main/java/net/badbird5907/jdacommand/Command.java
+++ src/main/java/net/badbird5907/jdacommand/Command.java
@@ -1,11 +1,13 @@
 package net.badbird5907.jdacommand;
 
 public abstract class Command {
-    protected String name = "null";
-    protected String desc = "null";
-    public Command(String name, String desc){
-        this.name = name;
-        this.desc = desc;
-    }
-    protected abstract void execute(String[] args, CommandEvent event);
+	protected String name;
+	protected String desc;
+
+	public Command(String name, String desc) {
+		this.name = name;
+		this.desc = desc;
+	}
+
+	protected abstract void execute(String[] args, CommandEvent event);
 }
--- src/main/java/net/badbird5907/jdacommand/MessageListener.java
+++ src/main/java/net/badbird5907/jdacommand/MessageListener.java
@@ -3,11 +3,13 @@ package net.badbird5907.jdacommand;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.Arrays;
+import static java.lang.System.out;
+import static net.badbird5907.jdacommand.JDACommand.commands;
+import static net.badbird5907.jdacommand.JDACommand.getInstance;
 
 public class MessageListener extends ListenerAdapter {
-    @Override
-    public void onMessageReceived(MessageReceivedEvent e){
+	@Override
+	public void onMessageReceived(MessageReceivedEvent e) {
         /*
         System.out.println(e.getMessage().getContentRaw());
         if(e.getMessage().getContentRaw().startsWith(JDACommand.getInstance().prefix)){
@@ -29,17 +31,9 @@ public class MessageListener extends ListenerAdapter {
             });
         }
          */
-        System.out.println(e.getMessage().getContentRaw());
-        if(!e.getMessage().getContentRaw().startsWith(JDACommand.getInstance().prefix))
-            return;
-        String[] args1 = e.getMessage().getContentRaw().split(" ");
-        String a = e.getMessage().getContentRaw().replaceFirst(args1[0], "");
-        String[] args = a.split(" ");
-        JDACommand.commands.forEach(cmd ->{
-            if(cmd.name.equalsIgnoreCase(args1[0].replaceFirst(JDACommand.getInstance().prefix, ""))){
-                cmd.execute(args, new CommandEvent(args, e.getChannel(), e.getMessage(), e));
-            }
-        });
-    }
-
+		out.println(e.getMessage().getContentRaw());
+		if (!e.getMessage().getContentRaw().startsWith(getInstance().prefix)) return;
+		String[] args1 = e.getMessage().getContentRaw().split(" "), args = e.getMessage().getContentRaw().replaceFirst(args1[0], "").split(" ");
+		commands.stream().filter(cmd -> cmd.name.equalsIgnoreCase(args1[0].replaceFirst(getInstance().prefix, ""))).forEach(cmd -> cmd.execute(args, new CommandEvent(args, e.getChannel(), e.getMessage(), e)));
+	}
 }
